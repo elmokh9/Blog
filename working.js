3d6224f9ed97864425a526b2267c7f280d260a2d
Eshan Singh Awal <singheshan29@gmail.com>
2022-01-13 18:58:14 +0530

    old version

---
+++ working.js
@@ -0,0 +1,21 @@
+//things to do
+//responsive text vw
+//navbar responsive
+
+document.querySelector("button").onclick = function(){
+    window.alert("hola");
+}
+var workEl = document.querySelector("#workDiv");
+    workEl.onclick = function(){
+        window.alert("hello");
+        document.querySelector("#work").style.background = "yellow";
+    }
+document.querySelector("button").onmouseover
+
+$(document).ready(function() {
+    $(".fa").click(open);    
+})
+
+function open(){
+    $(".hamburgerMenu").width("100%");
+}
