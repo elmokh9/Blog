10cea674d0e43cba3ab614734a498e775bfb2bdb
Eshan Singh Awal <singheshan29@gmail.com>
2022-01-30 11:39:38 +0530

    minorChanges

--- working.js
+++ working.js
@@ -1,17 +1,7 @@
-//things to do
-//responsive text vw
-//navbar responsive
 
 document.querySelector("button").onclick = function(){
     window.alert("hola");
 }
-
-// var workEl = document.querySelector("#workDiv");
-//     workEl.onclick = function(){
-//         window.alert("hello");
-//         document.querySelector("#work").style.background = "yellow";
-//     }
-// document.querySelector("button").onmouseover
 
 $(document).ready(function() {
     changeOrder();
@@ -62,3 +52,10 @@ window.matchMedia(width).addEventListener("change", changeOrder);
 function open(){
     $(".hamburgerMenu").width("100%");
 }
+
+// var workEl = document.querySelector("#workDiv");
+//     workEl.onclick = function(){
+//         window.alert("hello");
+//         document.querySelector("#work").style.background = "yellow";
+//     }
+// document.querySelector("button").onmouseover
